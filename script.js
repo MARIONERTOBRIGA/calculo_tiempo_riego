11f1c3eb8b2a968e519af4b6b39dcbf3738d05de
MARIONERTOBRIGA <161958812+MARIONERTOBRIGA@users.noreply.github.com>
2024-03-02 22:10:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,11 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const distanciaGoteros = parseFloat(document.getElementById('distancia-goteros').value);
     const caudalGoteros = parseFloat(document.getElementById('caudal-goteros').value);
 
-    // Calcular el área de riego (hectáreas)
-    const areaRiego = (distanciaFilas * distanciaGoteros) / 10000;
-
     // Calcular el tiempo de riego (horas)
-    const tiempoRiego = volumenRiego / (caudalGoteros * areaRiego);
+    const tiempoRiego = volumenRiego / (10000 / (distanciaFilas * distanciaGoteros / 100) * caudalGoteros / 1000);
 
     // Mostrar el resultado en el contenedor de resultado
     resultContainer.innerHTML = `Tiempo de riego: ${tiempoRiego.toFixed(2)} horas`;
